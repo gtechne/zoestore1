572926b2fe7d4a5dd9f85979e456e650831ef970
EXQUISITE GROUP <adexgodwilli@gmail.com>
2024-10-29 01:29:32 +0100

    firebase update

--- servers.js
+++ servers.js
@@ -7,7 +7,9 @@ const axios = require("axios");
 const app = express();
 const path = require("path");
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = JSON.parse(
+  Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT_KEY, 'base64').toString('utf-8')
+);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
